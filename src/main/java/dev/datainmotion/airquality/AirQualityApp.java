6c2a60c349a95164719dd80ac7e8d7c7a14c7407
tspannhw <tim.spann@streamnative.io>
2022-04-08 08:55:49 -0400

    Update

--- src/main/java/dev/datainmotion/airquality/AirQualityApp.java
+++ src/main/java/dev/datainmotion/airquality/AirQualityApp.java
@@ -12,12 +12,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 /** 
  * example spring boot app to read rest feed send to Pulsar 
  * 
 */
 @SpringBootApplication
+@EnableScheduling
 public class AirQualityApp implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(AirQualityApp.class);
@@ -29,8 +34,10 @@ public class AirQualityApp implements CommandLineRunner {
     @Autowired
     private AirQualityService airQualityService;
 
+	@Scheduled(initialDelay = 10, fixedRate = 10000)
 	@Override
     public void run(String... args) {
+		logger.error("Current time is :: " + Calendar.getInstance().getTime());
 		List<Observation> obsList = airQualityService.fetchCurrentObservation();
 		MessageId msgId = null;
 		for (Observation observation2 : obsList) {
