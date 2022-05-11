ae9a0a509ed00db8eac2b1f81cf8fe4830096e88
tspannhw <tim.spann@streamnative.io>
2022-05-11 13:39:53 -0400

    Update

--- src/test/java/dev/datainmotion/airquality/AirQualityAppTests.java
+++ src/test/java/dev/datainmotion/airquality/AirQualityAppTests.java
@@ -1,9 +1,28 @@
 package dev.datainmotion.airquality;
 
+import dev.datainmotion.airquality.model.Observation;
+import dev.datainmotion.airquality.service.AirQualityService;
+import dev.datainmotion.airquality.service.PulsarService;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 public class AirQualityAppTests {
 
+    @Autowired
+    PulsarClient pulsarClient;
 
+    @Autowired
+    Producer<Observation> producer;
+
+    @Autowired
+    private PulsarService pulsarService;
+
+    @Test
+    void contextLoads() {
+    }
 }
