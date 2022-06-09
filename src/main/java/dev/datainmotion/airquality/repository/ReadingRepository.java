f5e097d1355dac9e6df201b06a68ab44855cbdc9
tspannhw <tim.spann@streamnative.io>
2022-06-09 08:53:30 -0400

    Update

--- src/main/java/dev/datainmotion/airquality/repository/ReadingRepository.java
+++ src/main/java/dev/datainmotion/airquality/repository/ReadingRepository.java
@@ -11,9 +11,8 @@ import java.util.Optional;
  */
 public interface ReadingRepository extends CrudRepository<Reading, String> {
     Optional<Reading> findByReadingID(String readingID);
-    Optional<Reading> findByState_Code(String state_code);
-    Optional<Reading> findByMax_Pm10(String max_pm10);
-    Optional<Reading> findByMax_Ozone(String max_ozone);
-    Optional<Reading> findByMax_Pm25(String max_pm25);
-//    List<Reading> findAll();
+    Optional<Reading> findByStateCode(String state_code);
+    Optional<Reading> findByMaxPm10(String max_pm10);
+    Optional<Reading> findByMaxOzone(String max_ozone);
+    Optional<Reading> findByMaxPm25(String max_pm25);
 }
